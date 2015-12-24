69e2fde6da59a201e0705f4a60a0560df47a6f6e
Niklas Hofer <niklas+dev@lanpartei.de>
2015-12-24 03:12:13 +0100

    remove single LED code

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,6 @@
 #include <FastLED.h>
 #include <Fire.h>
 
-#define LED_R 6
-#define LED_G 5
-#define LED_B 3
-
 #define LED_TYPE LPD8806
 #define STRIP_COLOR_ORDER BRG
 #define STRIP_PIXEL_COUNT 12
@@ -35,22 +31,13 @@ byte cooling, sparking, base;
 int step;
 
 byte chan;
-int dir;
-int chill;
 uint8_t dryness;
-
-void setLED(CRGB color) {
-  analogWrite(LED_R, color.red);
-  analogWrite(LED_G, color.green);
-  analogWrite(LED_B, color.blue);
-}
 
 void setup() {
 #ifdef DEBUG
   Serial.begin(9600);
 #endif
   color = CRGB::Black;
-  setLED(color);
 
   FastLED.addLeds<LED_TYPE,PIN_STRIP_DATA,PIN_STRIP_CLK,STRIP_COLOR_ORDER>(strip, STRIP_PIXEL_COUNT).setCorrection(TypicalLEDStrip);
   FastLED.setMaxRefreshRate(FPS);
@@ -68,18 +55,8 @@ void setup() {
   dryness = 0xFF;
 }
 
-void animationInit() {
-  chan = random(3);
-  dir  = color[chan] <= MIN ? 1 : -1;
-  step = color[chan];
-  chill = random(CHILL) + CHILL/4;
-}
-
 void animationStep() {
   int i;
-  color[chan] = step;
-  chill -= 1;
-
   Fire__eachStep(heat, STRIP_PIXEL_COUNT, cooling, sparking, base);
 
   for (i; i<STRIP_PIXEL_COUNT; i++) {
@@ -92,7 +69,6 @@ void animationStep() {
 }
 
 void animationNext() {
-  step = step + dir;
   delay(1000/FPS);
 }
 
@@ -120,10 +96,6 @@ void readDirt() {
 
 void loop() {
   FastLED.show();
-  setLED(color);
-  if ( (step <= MIN) || (step >= MAX) || (chill == 0) )  { // Start of animation
-    animationInit();
-  }
   animationNext();
   animationStep();
 
